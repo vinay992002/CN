784d5df172c394c0df96c9cbac463214fdc0e8b6
vinay992002 <vinay992002@yahoo.com>
2016-02-14 01:02:23 +0530

    added hamming's code

---
+++ Hammingscode.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
+#define parity 1  //1 for odd parity and 2 for even parity
+int main(){
+	string s;
+	cin>>s;
+	int nc = s.length(); 
+	long long int data = 0;
+	int y;
+	//cout<<nc<<endl;
+	for(int i=0;i<nc;i++)
+	{
+		data<<=1;
+		y = s[i]-'0';
+		data = data|y;
+	}
+	//cout<<data<<endl;
+	int r=0;
+	while(pow(2,r)< nc+r+1)r++;
+	map<int,vector<int> > m;
+	for(int i=0;i<r;i++)
+	{
+		for(int j=1;j<nc+r;j++)
+		{
+			if(CHECK_BIT(j,i))
+			{
+				m[i+1].push_back(j);
+			}
+		}
+	}
+	long long int mdata=0;
+	int x=0;
+	int temp;
+	for(int i=2;i<=nc+r;i++)
+	{
+
+		if((i&(i-1)) != 0)
+		{
+
+			temp=0;
+			if(CHECK_BIT(data,x))
+			{
+				temp=1;
+				temp<<=(i-1);
+			}
+			mdata = mdata|temp;
+			x++;
+		}
+	}
+	//cout<<x<<"   ";
+	//cout<<mdata<<endl;
+	map<int ,vector<int> > ::iterator it;
+	vector<int> vec;
+	int count ;
+	for(it = m.begin();it!=m.end();it++)
+	{
+		vec = it->second;
+		count =0;
+		//cout<<it->first<<"-->";
+		for(int i=0;i<vec.size();i++)
+		{
+			if(CHECK_BIT(mdata,vec[i]-1))
+			{
+				count++;
+			}
+			
+		}
+		//cout<<count<<" ";
+		//cout<<endl;
+		if(parity==1 && count%2==0 )
+		{
+			temp=1;
+			temp= temp << (int)(pow(2,it->first-1)-1);
+			mdata = mdata|temp;
+		}
+		else if(parity==2 && count%2!=0)
+		{
+			temp=1;
+			temp= temp << (int)(pow(2,it->first-1)-1);
+			mdata = mdata|temp;
+		}
+		//cout<<mdata<<" ";
+	}
+	long long int rdata=mdata;
+	vec.clear();
+	//cout<<"\n\n";
+	while(rdata>0)
+	{
+		y = rdata&1;
+		vec.push_back(y);
+		rdata>>=1;
+	}	
+	for(int i=vec.size()-1;i>=0;i--)
+	{
+		cout<<vec[i];
+	}
+	cout<<endl;
+}
